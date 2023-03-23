1db31570b0a337d2358c94792ec5aa3c2e973f94
Catriel <cartup90@gmail.com>
2023-03-23 16:23:18 -0300

    Dinamismo al título, agrego listado de colores

---
+++ titulos.js
@@ -0,0 +1,32 @@
+const posiblesTitulos = [
+    "Construyo experiencias digitales de impacto",
+    "Diseño soluciones web a medida",
+    "Transformo ideas en realidad digital"
+  ];
+  
+  function escribirTitulo(titulo, elemento, tiempo) {  //Añade el título letra por letra
+    return new Promise(resolve => {
+      let indice = 0;
+      elemento.textContent = "";
+      const intervalo = setInterval(() => {
+        if (indice < titulo.length) {
+          elemento.textContent += titulo[indice];
+          indice++;
+        } else {
+          clearInterval(intervalo);
+          setTimeout(resolve, tiempo);
+        }
+      }, tiempo);
+    });
+  }
+  
+  async function cambiarTitulo() {
+    const tituloActual = document.getElementById("titulo-dinamico").textContent;
+    const indiceTitulo = posiblesTitulos.indexOf(tituloActual);
+    const nuevoTitulo = posiblesTitulos[(indiceTitulo + 1) % posiblesTitulos.length];
+    const tituloElemento = document.getElementById("titulo-dinamico");
+    await escribirTitulo(nuevoTitulo, tituloElemento, 50);
+    tituloElemento.textContent = nuevoTitulo;
+  }
+  
+  setInterval(cambiarTitulo, 4500); // Cambia el título cada 5 segundos
